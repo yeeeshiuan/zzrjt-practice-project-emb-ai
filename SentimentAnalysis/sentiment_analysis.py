77f5f5243893cdf29f24017d60869c8272b86542
Joey Huang <yeeeshiuan.huang@gmail.com>
2024-04-16 22:53:19 -0400

    [main]     fix the issues from pylint to sentiment_analysis.py

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,10 +1,19 @@
-import requests
+'''
+sentiment analysis API tool function
+'''
+
 import json
+import requests
+
 def sentiment_analyzer(text_to_analyse):
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    '''
+    sentiment analyzer by API calling
+    '''
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/'
+    url = url + 'v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    response = requests.post(url, json = myobj, headers=header)
+    response = requests.post(url, json = myobj, headers=header, timeout=3)
     formatted_response = json.loads(response.text)
     if response.status_code == 200:
         label = formatted_response['documentSentiment']['label']
